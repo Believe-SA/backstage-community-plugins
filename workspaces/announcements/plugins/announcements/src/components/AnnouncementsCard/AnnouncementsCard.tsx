6f344db759eb4e7536e5d6dd5b2446f695d07db4
Gabriel Dugny <gabriel.dugny@believe.com>
2025-04-15 10:28:18 +0200

    feat(announcements): Add support for customizable homepage
    Signed-off-by: Gabriel Dugny <gabriel.dugny@believe.com>
    
    # Conflicts:
    #	workspaces/announcements/plugins/announcements/src/components/AnnouncementsCard/AnnouncementsCard.tsx
    
    # Conflicts:
    #	workspaces/announcements/plugins/announcements/package.json
    #	workspaces/announcements/plugins/announcements/src/components/AnnouncementsCard/AnnouncementsCard.tsx
    
    # Conflicts:
    #	workspaces/announcements/plugins/announcements/package.json

--- workspaces/announcements/plugins/announcements/src/components/AnnouncementsCard/AnnouncementsCard.tsx
+++ workspaces/announcements/plugins/announcements/src/components/AnnouncementsCard/AnnouncementsCard.tsx
@@ -13,49 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { DateTime } from 'luxon';
-import { usePermission } from '@backstage/plugin-permission-react';
-import {
-  InfoCard,
-  InfoCardVariants,
-  Link,
-  Progress,
-} from '@backstage/core-components';
-import { useApi, useRouteRef } from '@backstage/core-plugin-api';
-import { announcementEntityPermissions } from '@backstage-community/plugin-announcements-common';
-import {
-  announcementCreateRouteRef,
-  announcementViewRouteRef,
-  rootRouteRef,
-} from '../../routes';
-import { formatAnnouncementStartTime } from '../utils/announcementDateUtils';
-import {
-  announcementsApiRef,
-  useAnnouncements,
-  useAnnouncementsTranslation,
-} from '@backstage-community/plugin-announcements-react';
-import {
-  makeStyles,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Typography,
-  Box,
-  Chip,
-} from '@material-ui/core';
-import { Alert } from '@material-ui/lab';
-import NewReleasesIcon from '@material-ui/icons/NewReleases';
-
-const useStyles = makeStyles({
-  newAnnouncementIcon: {
-    minWidth: '36px',
-  },
-  chipStyle: {
-    marginRight: 4,
-    marginBottom: 4,
-  },
-});
+import React from 'react';
+import { InfoCard, InfoCardVariants } from '@backstage/core-components';
+import { useRouteRef } from '@backstage/core-plugin-api';
+import { useAnnouncementsTranslation } from '@backstage-community/plugin-announcements-react';
+import { AnnouncementsList } from './AnnouncementsList';
+import { rootRouteRef } from '../../routes';
 
 type AnnouncementsCardOpts = {
   title?: string;
@@ -80,34 +43,8 @@ export const AnnouncementsCard = ({
   current,
   hideStartAt,
 }: AnnouncementsCardOpts) => {
-  const classes = useStyles();
-  const announcementsApi = useApi(announcementsApiRef);
-  const announcementsLink = useRouteRef(rootRouteRef);
-  const viewAnnouncementLink = useRouteRef(announcementViewRouteRef);
-  const createAnnouncementLink = useRouteRef(announcementCreateRouteRef);
-  const lastSeen = announcementsApi.lastSeenDate();
   const { t } = useAnnouncementsTranslation();
-
-  const { announcements, loading, error } = useAnnouncements({
-    max: max || 5,
-    category,
-    active,
-    sortBy,
-    order,
-    current,
-  });
-
-  const { announcementCreatePermission } = announcementEntityPermissions;
-  const { loading: loadingPermission, allowed: canAdd } = usePermission({
-    permission: announcementCreatePermission,
-  });
-
-  if (loading) {
-    return <Progress />;
-  } else if (error) {
-    return <Alert severity="error">{error.message}</Alert>;
-  }
-
+  const announcementsLink = useRouteRef(rootRouteRef);
   const deepLink = {
     link: announcementsLink(),
     title: t('announcementsCard.seeAll'),
@@ -119,93 +56,15 @@ export const AnnouncementsCard = ({
       variant={variant}
       deepLink={deepLink}
     >
-      <List dense>
-        {announcements.results.map(announcement => (
-          <ListItem key={announcement.id}>
-            <ListItemIcon
-              className={classes.newAnnouncementIcon}
-              style={{
-                visibility:
-                  lastSeen < DateTime.fromISO(announcement.created_at)
-                    ? 'visible'
-                    : 'hidden',
-              }}
-              title={t('announcementsCard.new')}
-            >
-              <NewReleasesIcon />
-            </ListItemIcon>
-            <ListItemText
-              primary={
-                <Link
-                  to={viewAnnouncementLink({ id: announcement.id })}
-                  variant="inherit"
-                >
-                  {announcement.title}
-                </Link>
-              }
-              secondary={
-                <Box>
-                  <Typography variant="body2" color="textSecondary">
-                    {DateTime.fromISO(announcement.created_at).toRelative()}
-                    {announcement.category && (
-                      <>
-                        {` ${t('announcementsCard.in')} `}
-                        <Link
-                          to={`${announcementsLink()}?category=${
-                            announcement.category.slug
-                          }`}
-                          variant="inherit"
-                        >
-                          {announcement.category.title}
-                        </Link>
-                      </>
-                    )}
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary">
-                    {announcement.excerpt}
-                    {announcement.tags && announcement.tags.length > 0 && (
-                      <Box mt={1}>
-                        {announcement.tags.map(tag => (
-                          <Chip
-                            key={tag.slug}
-                            size="small"
-                            label={tag.title}
-                            component={Link}
-                            to={`${announcementsLink()}?tags=${tag.slug}`}
-                            clickable
-                            className={classes.chipStyle}
-                          />
-                        ))}
-                      </Box>
-                    )}
-                  </Typography>
-                  {!hideStartAt && (
-                    <Typography variant="caption" color="textSecondary">
-                      {formatAnnouncementStartTime(
-                        announcement.start_at,
-                        t('announcementsCard.occurred'),
-                        t('announcementsCard.scheduled'),
-                        t('announcementsCard.today'),
-                      )}
-                    </Typography>
-                  )}
-                </Box>
-              }
-            />{' '}
-          </ListItem>
-        ))}
-        {announcements.count === 0 && !loadingPermission && canAdd && (
-          <ListItem>
-            <ListItemText>
-              {`${t('announcementsCard.noAnnouncements')} `}
-              <Link to={createAnnouncementLink()} variant="inherit">
-                {t('announcementsCard.addOne')}
-              </Link>
-              ?
-            </ListItemText>
-          </ListItem>
-        )}
-      </List>
+      <AnnouncementsList
+        max={max}
+        category={category}
+        active={active}
+        sortBy={sortBy}
+        order={order}
+        current={current}
+        hideStartAt={hideStartAt}
+      />
     </InfoCard>
   );
 };
